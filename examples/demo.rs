92419bc1e1a745cfa7cc6c4453764db03d2b5f24
Ken <kenakioreed@gmail.com>
2021-02-15 21:52:23 -0500

    cleanup

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,37 +1,36 @@
-
 use tagger::prelude::*;
 
-fn main(){
-    
-    let mut string=String::new();
+fn main() {
+    let mut string = String::new();
 
     {
-        let mut root=tagger::root(&mut string);
+        let mut root = tagger::root(&mut string);
         root.declaration("DOCTYPE html");
-        
-        let mut html=root.tag_build("html").end();
-        
+
+        let mut html = root.tag_build("html").end();
+
         //html.tag_build("rect").append("class='poloto2fill' height='7.5' rx='5' ry='5' width='50' x='680' y='176.25'").empty();
-        
-        
-        html.tag_build("rect").set("width",4).empty_no_slash();
+
+        html.tag_build("rect").set("width", 4).empty_no_slash();
         html.comment("test comment!");
-        
-        html.tag_build("rect").set("class","poloto2fill").set("height",7.5).set("rx",5).empty();
-        
-        let mut style=html.tag_build("style").end();
+
+        html.tag_build("rect")
+            .set("class", "poloto2fill")
+            .set("height", 7.5)
+            .set("rx", 5)
+            .empty();
+
+        let mut style = html.tag_build("style").end();
         style.write_str(".potato{chicken}\n");
         drop(style);
-        
-        let mut div=html.tag_build("div").append("x=5").end();
+
+        let mut div = html.tag_build("div").append("x=5").end();
         div.tag_build("svg").append("foo").end();
         div.tag_build("svg").append("blag").end();
         div.tag_build("img").append("width='100%'").empty();
         drop(div);
 
-
         html.tag_build("div").append("kiki=7").end();
-        
     }
     use std::io::Write;
     std::io::stdout().write_all(&string.as_bytes()).unwrap();
--- examples/demo2.rs
+++ examples/demo2.rs
@@ -1,20 +1,18 @@
-
 use tagger::prelude::*;
 
-fn main(){
-    let mut s=String::new();
+fn main() {
+    let mut s = String::new();
 
     {
-        let root=tagger::root(&mut s);
-        
-        let svg=root.tag_build_flat("svg").set("width",100).end();
-        let svg=svg.tag_build_flat("img").set("height",100).empty();
-        let div=svg.tag_build_flat("div").end();
-        let svg=div.pop();
-        let mut svg=svg.tag_build_flat("chicken").empty();
-        svg.tag_build("potato").set("foo",5).empty();
+        let root = tagger::root(&mut s);
+
+        let svg = root.tag_build_flat("svg").set("width", 100).end();
+        let svg = svg.tag_build_flat("img").set("height", 100).empty();
+        let div = svg.tag_build_flat("div").end();
+        let svg = div.pop();
+        let mut svg = svg.tag_build_flat("chicken").empty();
+        svg.tag_build("potato").set("foo", 5).empty();
     }
 
-    println!("{}",s);
-
+    println!("{}", s);
 }
