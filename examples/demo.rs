5c58f3bc3930598825f77f22743211bbc8fd3987
Ken <kenakioreed@gmail.com>
2021-02-16 14:39:42 -0500

    cargo fmt

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,25 +1,26 @@
-
-
-use tagger::*;
 use core::fmt;
+use tagger::*;
 //TODO make path data / polyline data implement Display.
 //THEN you can pass it to the formatter!!!!
 
-fn main()->Result<(),fmt::Error> {
+fn main() -> Result<(), fmt::Error> {
     let mut string = String::new();
     {
-        
-        let mut k=new_element!(&mut string,"<rect x={},y={}>","</rect>",4,5)?;
-        
-        let p=poly((0..5).map(|x|[x as f32,x as f32]));
+        let mut k = new_element!(&mut string, "<rect x={},y={}>", "</rect>", 4, 5)?;
 
-        
-        let mut j=element!(k,"<svg x={} y={} points={polydata}>","</svg>",5,4,polydata=p)?;
-        empty_element!(j,"<chicken>")?;
+        let p = poly((0..5).map(|x| [x as f32, x as f32]));
 
-        
+        let mut j = element!(
+            k,
+            "<svg x={} y={} points={polydata}>",
+            "</svg>",
+            5,
+            4,
+            polydata = p
+        )?;
+        empty_element!(j, "<chicken>")?;
     }
-    println!("{}",string);
+    println!("{}", string);
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,71 @@
-
-
-pub mod prelude{
-
-}
+pub mod prelude {}
 pub use inner::Element;
 pub use inner::Single;
-mod inner{
-    use core::fmt::Write;
+mod inner {
     use core::fmt;
+    use core::fmt::Write;
 
-    pub struct Single<'a,T:Write>{
-        writer:&'a mut T
+    pub struct Single<'a, T: Write> {
+        writer: &'a mut T,
     }
-    impl<'a,T:Write> Single<'a,T>{
-        pub fn borrow<'b>(&'b mut self,a:fmt::Arguments<'_>,b:&'b str)->Result<Element<'b,T>,fmt::Error>{
+    impl<'a, T: Write> Single<'a, T> {
+        pub fn borrow<'b>(
+            &'b mut self,
+            a: fmt::Arguments<'_>,
+            b: &'b str,
+        ) -> Result<Element<'b, T>, fmt::Error> {
             self.writer.write_fmt(a)?;
-            Ok(Element{
-                writer:self.writer,
-                func:Some(b)
+            Ok(Element {
+                writer: self.writer,
+                func: Some(b),
             })
         }
     }
-    pub struct Element<'a,T:Write>{
-        writer:&'a mut T,
-        func:Option<&'a str>
+    pub struct Element<'a, T: Write> {
+        writer: &'a mut T,
+        func: Option<&'a str>,
     }
-    impl<'a,T:Write> Drop for Element<'a,T>{
-        fn drop(&mut self){
-            
+    impl<'a, T: Write> Drop for Element<'a, T> {
+        fn drop(&mut self) {
             let _ = self.writer.write_str(self.func.take().unwrap());
         }
     }
-    impl<'a,T:Write> Element<'a,T>{
-        pub fn new(writer:&'a mut T,ar:fmt::Arguments,func2:&'a str)->Result<Self,fmt::Error>{
+    impl<'a, T: Write> Element<'a, T> {
+        pub fn new(
+            writer: &'a mut T,
+            ar: fmt::Arguments,
+            func2: &'a str,
+        ) -> Result<Self, fmt::Error> {
             writer.write_fmt(ar)?;
-            Ok(Element{
+            Ok(Element {
                 writer,
-                func:Some(func2)
+                func: Some(func2),
             })
         }
-        pub fn write_str(&mut self,s:&str)->fmt::Result{
-            write!(self.writer,"{}",s)
+        pub fn write_str(&mut self, s: &str) -> fmt::Result {
+            write!(self.writer, "{}", s)
         }
-        pub fn get_writer(&mut self)->&mut T{
+        pub fn get_writer(&mut self) -> &mut T {
             &mut self.writer
         }
-        pub fn borrow<'b>(&'b mut self,a:fmt::Arguments,b:&'a str)->Result<Element<'b,T>,fmt::Error>{
+        pub fn borrow<'b>(
+            &'b mut self,
+            a: fmt::Arguments,
+            b: &'a str,
+        ) -> Result<Element<'b, T>, fmt::Error> {
             self.writer.write_fmt(a)?;
-            Ok(Element{
-                writer:self.writer,
-                func:Some(b)
+            Ok(Element {
+                writer: self.writer,
+                func: Some(b),
             })
         }
-        pub fn borrow_single<'b>(&'b mut self,a:fmt::Arguments)->Result<Single<'b,T>,fmt::Error>{
+        pub fn borrow_single<'b>(
+            &'b mut self,
+            a: fmt::Arguments,
+        ) -> Result<Single<'b, T>, fmt::Error> {
             self.writer.write_fmt(a)?;
-            Ok(Single{
-                writer:self.writer
+            Ok(Single {
+                writer: self.writer,
             })
         }
     }
@@ -89,68 +99,64 @@ macro_rules! empty_element {
     }
 }
 
-
 use core::fmt;
 
-
-pub struct PathCommander<'a,'b>{
-    writer:&'a mut fmt::Formatter<'b>
+pub struct PathCommander<'a, 'b> {
+    writer: &'a mut fmt::Formatter<'b>,
 }
 
-impl<'a,'b> PathCommander<'a,'b>{
-    pub fn close(&mut self)->fmt::Result {
+impl<'a, 'b> PathCommander<'a, 'b> {
+    pub fn close(&mut self) -> fmt::Result {
         write!(self.writer, "z")
     }
-    pub fn move_to(&mut self, point: [f32; 2]) -> Result<&mut Self,fmt::Error> {
+    pub fn move_to(&mut self, point: [f32; 2]) -> Result<&mut Self, fmt::Error> {
         write!(self.writer, "M {} {} ", point[0], point[1])?;
         Ok(self)
     }
-    pub fn line_to(&mut self, point: [f32; 2]) -> Result<&mut Self,fmt::Error> {
+    pub fn line_to(&mut self, point: [f32; 2]) -> Result<&mut Self, fmt::Error> {
         write!(self.writer, "L {} {} ", point[0], point[1])?;
         Ok(self)
     }
 }
 
-pub fn path(func:impl FnOnce(PathCommander)->fmt::Result)->impl fmt::Display{
-    struct Path<F:FnOnce(PathCommander)->fmt::Result>{
-        it:std::cell::RefCell<Option<F>>
+pub fn path(func: impl FnOnce(PathCommander) -> fmt::Result) -> impl fmt::Display {
+    struct Path<F: FnOnce(PathCommander) -> fmt::Result> {
+        it: std::cell::RefCell<Option<F>>,
     }
-    impl<F:FnOnce(PathCommander)->fmt::Result> fmt::Display for Path<F>{
+    impl<F: FnOnce(PathCommander) -> fmt::Result> fmt::Display for Path<F> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            let comm=PathCommander{writer:f};
+            let comm = PathCommander { writer: f };
             (self.it.borrow_mut().take().unwrap())(comm)?;
             Ok(())
         }
     }
 
-    Path{
-        it:std::cell::RefCell::new(Some(func))
+    Path {
+        it: std::cell::RefCell::new(Some(func)),
     }
 }
 
-pub fn poly(a:impl ExactSizeIterator<Item=[f32;2]>)->impl fmt::Display{
-    struct PolyLine<I>{
-        it:std::cell::RefCell<I>
+pub fn poly(a: impl ExactSizeIterator<Item = [f32; 2]>) -> impl fmt::Display {
+    struct PolyLine<I> {
+        it: std::cell::RefCell<I>,
     }
-    impl<I:Iterator<Item=[f32;2]>> PolyLine<I>{
-        fn new(it:I)->PolyLine<I>{
-            PolyLine{
-                it:std::cell::RefCell::new(it)
+    impl<I: Iterator<Item = [f32; 2]>> PolyLine<I> {
+        fn new(it: I) -> PolyLine<I> {
+            PolyLine {
+                it: std::cell::RefCell::new(it),
             }
         }
     }
-    impl<I:Iterator<Item=[f32;2]>> fmt::Display for PolyLine<I>{
+    impl<I: Iterator<Item = [f32; 2]>> fmt::Display for PolyLine<I> {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            for [x,y] in &mut *self.it.borrow_mut(){
-                write!(f,"{},{} ",x,y)?
+            for [x, y] in &mut *self.it.borrow_mut() {
+                write!(f, "{},{} ", x, y)?
             }
             Ok(())
         }
     }
     PolyLine::new(a)
 }
-
-
 
 ///Used by [`upgrade_writer`]
 pub struct WriterAdaptor<T> {
@@ -176,5 +182,3 @@ impl<T: std::io::Write> fmt::Write for WriterAdaptor<T> {
         }
     }
 }
-
-
