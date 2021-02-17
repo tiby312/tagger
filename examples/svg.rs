d83ba6e139a4af822c4d6aba0626f97ac856fdf0
Ken <kenakioreed@gmail.com>
2021-02-17 08:57:54 -0500

    cleanup

--- examples/svg.rs
+++ examples/svg.rs
@@ -5,15 +5,6 @@ fn main() -> core::fmt::Result {
     let width = 100.0;
     let height = 100.0;
 
-    {
-        let mut s=String::new();
-        let mut stack=tagger::elem::ElementStack::new(s,format_args!("<div>"),"</div>")?;
-        stack.borrow_move(format_args!("<p>"),"</p>")?;
-
-        stack.end_last()?;
-
-        stack.end_last()?;
-    }
     let mut svg = new_element!(
         &mut io,
         "<svg viewBox='0 0 {} {}' xmlns='http://www.w3.org/2000/svg'>",
@@ -39,6 +30,8 @@ fn main() -> core::fmt::Result {
     for r in (0..50).step_by(10) {
         empty_element!(g, "<circle cx='{}' cy='{}' r='{}'/>", 50.0, 50.0, r)?;
     }
+
+    //Program panics if the elements aren't closed.
     end!(g, "</g>")?;
 
     end!(svg, "</svg>")?;
