f2db8e85519bb0621f9eee68e36ed57ae2f2c968
Ken <kenakioreed@gmail.com>
2021-02-19 21:18:43 -0500

    added raw example

---
+++ examples/raw.rs
@@ -0,0 +1,40 @@
+use tagger::prelude::*;
+
+fn main() -> core::fmt::Result {
+    let mut io = tagger::upgrade(std::io::stdout());
+    let width = 100.0;
+    let height = 100.0;
+
+    let mut svg = tagger::raw::Element::new(
+        &mut io,
+        wr!(
+            "<svg viewBox='0 0 {} {}' xmlns='http://www.w3.org/2000/svg'>",
+            width,
+            height
+        ),
+        wr!("</svg>"),
+    )?;
+
+    svg.single(wr!(
+        "{}",
+        "<style>.test{fill:none;stroke:white;stroke-width:3}</style>"
+    ))?;
+
+    svg.single(wr!(
+        "<rect width='{}' height='{}' rx='{}' yx='{}' style='fill:blue;'/>",
+        width,
+        height,
+        20,
+        20
+    ))?;
+
+    let mut g = svg.elem(wr!("<g class='test'>"), wr!("</g>"))?;
+    for r in (0..50).step_by(10) {
+        g.single(wr!("<circle cx='{}' cy='{}' r='{}'/>", 50.0, 50.0, r))?;
+    }
+
+    //Program panics if the elements aren't closed.
+    g.end()?;
+    svg.end()?;
+    Ok(())
+}
